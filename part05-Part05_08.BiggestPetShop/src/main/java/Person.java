dc21ffba8af2a462bb3c14e566658079d90054a1
Sven Sinowczik <sven@mbp-von-sven.localdomain>
2022-07-14 00:50:20 +0200

    Done

--- part05-Part05_08.BiggestPetShop/src/main/java/Person.java
+++ part05-Part05_08.BiggestPetShop/src/main/java/Person.java
@@ -19,7 +19,7 @@ public class Person {
 
     @Override
     public String toString() {
-        return this.nimi;
+        return this.name + ", has a frienc called " + pet.getName() +  "(" + pet.getBreed() + ")";
     }
 
 }

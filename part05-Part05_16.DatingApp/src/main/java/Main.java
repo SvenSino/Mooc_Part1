5080b38df1fe44773cf4afe87b5e8a1690cc2c50
Sven Sinowczik <sven@mbp-von-sven.localdomain>
2022-07-18 20:13:41 +0200

    Done

--- part05-Part05_16.DatingApp/src/main/java/Main.java
+++ part05-Part05_16.DatingApp/src/main/java/Main.java
@@ -3,5 +3,12 @@ public class Main {
 
     public static void main(String[] args) {
         // test your program here!
+        SimpleDate date = new SimpleDate(13, 2, 2015);
+        SimpleDate date1 = new SimpleDate(30, 12, 2015);
+        SimpleDate date2 = new SimpleDate(30, 11, 2015);
+        date.advance();
+        date1.advance();
+        date2.advance();
+        
     }
 }

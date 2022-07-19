74c0cadc42bcfeb0d187f727af3be241dd9524af
Sven Sinowczik <sven@mbp-von-sven.localdomain>
2022-07-19 23:45:44 +0200

    Done

--- part06-Part06_06.LongestInCollection/src/main/java/SimpleCollection.java
+++ part06-Part06_06.LongestInCollection/src/main/java/SimpleCollection.java
@@ -19,4 +19,19 @@ public class SimpleCollection {
         return this.elements;
     }
 
+ public String longest() {
+        if (this.elements.isEmpty()) {
+            return null;
+        }
+ 
+        String toReturn = this.elements.get(0);
+        for (String element : this.elements) {
+            if (element.length() > toReturn.length()) {
+                toReturn = element;
+            }
+        }
+ 
+        return toReturn;
+    }
+
 }
